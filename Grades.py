20fa83ebb0dc51d54f9a06353824b3f08e080e6b
Vikranth Pulamathi <vikranth1502@gmail.com>
2020-08-14 21:04:48 +0530

    Chap3_Pro2_Grades

---
+++ Grades.py
@@ -0,0 +1,14 @@
+list1 = [11, -21, -99, 2, 1 ,0]
+list2 = [12, -7, 5, 64, -14]
+list3 = [12, 14, -95, 3]
+for num in list1:
+    if num>=0:
+        print(num, end = " ")
+
+for num in list2:
+    if num>=0:
+        print(num, end = " ")
+
+for num in list3:
+    if num>=0:
+        print(num, end = " ")
